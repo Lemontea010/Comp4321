e6af1c6eba1428ef69c90a0fc41453a134f4b792
CKTOM <tom937919@gmail.com>
2024-03-21 20:40:01 +0800

    added document processing : stem and stopwords

---
+++ src/main/java/doccleaner.java
@@ -0,0 +1,67 @@
+
+import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Vector;
+
+public class doccleaner {
+
+    private Porter porter;
+    private HashSet<String> stopWords;
+
+    public doccleaner(String str){
+        super();
+        porter = new Porter();
+        stopWords = new HashSet<>();
+
+        BufferedReader br = null;
+        try{
+            FileReader fr = new FileReader(str);
+            br = new BufferedReader(fr);
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String line = null;
+        try {
+            while ((line = br.readLine()) != null) {
+                stopWords.add(line);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public Vector<String> cleanstopwords (Vector<String> content){
+        Vector<String> cleancontent = new Vector<>();
+        for(String element : content.toArray(new String[0])){
+            if(!stopWords.contains(element)){
+                cleancontent.add(element);
+            }
+        }
+        return cleancontent;
+    }
+    public Vector<String> stemcontent(Vector<String> content){
+        Vector<String> stem = new Vector<>();
+
+        //vector to string
+        StringBuilder sb = new StringBuilder();
+        for (String element : content) {
+            sb.append(element);
+            sb.append(" ");
+        }
+        String stemword = porter.stripAffixes(String.valueOf(sb));
+        //string to vector
+        String[] elements = stemword.split(" ");
+
+        for (String element : elements) {
+            stem.add(element);
+        }
+        return stem;
+    }
+}
--- src/main/java/web.java
+++ src/main/java/web.java
@@ -1,15 +1,19 @@
+import org.htmlparser.beans.StringBean;
+
+import java.util.Arrays;
 import java.util.Vector;
 
 public class web {
     private String url;
     private int id;
-
     private Vector<String> child_urls;
 
-    web(String _url,int _id,Vector<String> child){
-        url=_url;
-        id=_id;
-        child_urls=child;
+    private String parent_urls;
+    web(String _url,int _id,Vector<String> child , String parent){
+        this.url=_url;
+        this.id=_id;
+        this.child_urls=child;
+        this.parent_urls = parent;
     }
 
     String getUrl(){
@@ -24,4 +28,33 @@ public class web {
     void updateChild(Vector<String> child){
         child_urls=child;
     }
+
+    String getParent_urls(){return this.parent_urls;}
+
+    private Vector<String> extractWords()
+    {
+        // extract words in url and return them
+        // use StringTokenizer to tokenize the result from StringBean
+        StringBean sb;
+        sb = new StringBean();
+        sb.setLinks(true);
+        sb.setURL(this.url);
+        String text = sb.getStrings();
+        String[] tokens = text.split("[ ,?]+");
+        Vector<String> vec_tokens = new Vector<>(Arrays.asList(tokens));
+        return vec_tokens;
+    }
+
+    private Vector<String> wordprocessing(){
+
+        doccleaner dr = new doccleaner("stopwords.txt");
+        Vector<String> cleanedcontent = dr.cleanstopwords(this.extractWords());
+        cleanedcontent = dr.stemcontent(cleanedcontent);
+        return cleanedcontent;
+    }
+
+    void writefile(Vector<String> content){
+
+    }
 }
+
